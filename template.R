3ac84abf4b248f6bc1471ac09091f88d5e115ef7
samuel-rosa <alessandrosamuelrosa@gmail.com>
2018-01-04 14:13:01 -0200

    estrutura completamente reformulada

---
+++ template.R
@@ -0,0 +1,27 @@
+i <- 81
+glue('# {id[i]} {{-}}') %>% cat()
+glue('## Descrição geral {{-}}') %>% cat()
+dts[[i]][-1, ] %>%
+  pandoc.table(split.tables = Inf, justify = 'left', col.names = c('campo', 'valor'), row.names = FALSE)
+glue('## Distribuição espacial {{-}}') %>% print()
+n_obs <- nrow(obs[[i]])
+n_coords <- sum(!is.na(obs[[i]]$coord_x))
+if (n_coords != 0) {
+  tmp <- obs[[i]]
+  tmp <- tmp[!is.na(tmp$coord_x), ]
+  coordinates(tmp) <- ~ coord_x + coord_y
+  proj4string(tmp) <- CRS("+init=epsg:4674")
+  m <- 
+    mapview(
+      tmp, label = tmp$observacao_id, col.regions = "firebrick1", lwd = 1, col = "ivory", 
+      layer.name = id[i])@map %>% 
+    addMiniMap()
+  if (n_coords != n_obs) {
+    out <- n_obs - n_coords
+    glue("{out} observações do conjunto de dados {id[i]} não possuem coordenadas espaciais.") %>% 
+      cat()
+  }
+} else {
+  m <- glue("As observações do conjunto de dados {id[i]} não possuem coordenadas espaciais.") %>% cat()
+}
+m
